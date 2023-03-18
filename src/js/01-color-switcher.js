2beb10b861aad5022658406402f4dd7f3da3e853
Aleks-TO <satov69@gmail.com>
2023-03-18 10:47:58 +0200

    1-task-v1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
-const a = 2;
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+// отримаємо посилання на кнопки
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const bodyEl = document.querySelector('body');
+// console.log(bodyEl);
+
+// ставимо слухача подій на кнопки
+startBtn.addEventListener('click', handleChangeBg);
+
+function handleChangeBg() {
+  // поставимо таймер
+  const timerId = setInterval(() => {
+    const randomColor = getRandomHexColor();
+    bodyEl.style.backgroundColor = randomColor;
+  }, 1000);
+  return timerId;
+}
+
+stopBtn.addEventListener('click', () => clearInterval(timerId));
